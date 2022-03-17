f8bdfe8d060ef6c79e9afc2365430ecf5c0cb1dd
andiKandi <akraus931@gmail.com>
2022-03-17 19:09:43 +0100

    Citation

--- src/components/Page/Page.tsx
+++ src/components/Page/Page.tsx
@@ -1,73 +1,31 @@
-import React, { createContext, useMemo } from "react";
+import React from "react";
 import { Header } from "../Header/Header";
 import { Footer } from "../Footer/Footer";
-import { useDispatch, useSelector } from "react-redux";
-import { RootState } from "../../store/store";
-import { turnDark, turnLight } from "../../store/themeSlice";
-import { createTheme, PaletteMode, ThemeProvider } from "@mui/material";
-import { blueGrey, deepOrange } from "@mui/material/colors";
+import { createTheme, ThemeProvider } from "@mui/material";
 import "./Page.scss";
 
 interface PageProps {
   children: React.ReactNode;
 }
 
-const getDesignTokens = (mode: PaletteMode) => ({
-  palette: {
-    mode,
-    ...(mode === "light"
-      ? {
-          // palette values for light mode
-          primary: { main: blueGrey[900] },
-          divider: blueGrey[200],
-          text: {
-            primary: blueGrey[900],
-          },
-        }
-      : {
-          // palette values for dark mode
-          primary: deepOrange,
-          divider: deepOrange[700],
-          background: {
-            default: deepOrange[900],
-            paper: deepOrange[900],
-          },
-          text: {
-            primary: blueGrey[50],
-            secondary: blueGrey[500],
-          },
-        }),
-  },
-});
-
-const ColorModeContext = createContext({ toggleColorMode: () => {} });
-
 export const Page: React.FC<PageProps> = ({ children }) => {
-  const mode = useSelector((state: RootState) => state.theme.value);
-  const dispatch = useDispatch();
-
-  const colorMode = useMemo(
-    () => ({
-      // The dark mode switch would invoke this method
-      toggleColorMode: () => {
-        mode === "light" ? dispatch(turnDark()) : dispatch(turnLight());
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: "#191818",
       },
-    }),
-    [mode, dispatch]
-  );
-
-  // Update the theme only if the mode changes
-  // @ts-ignore TODO: set correct type!
-  const theme = useMemo(() => createTheme(getDesignTokens(mode)), [mode]);
+      secondary: {
+        main: "#C99E60",
+      },
+    },
+  });
   return (
     <>
-      <ColorModeContext.Provider value={colorMode}>
-        <ThemeProvider theme={theme}>
-          <Header />
-          <main className="main">{children}</main>
-          <Footer />
-        </ThemeProvider>
-      </ColorModeContext.Provider>
+      <ThemeProvider theme={theme}>
+        <Header />
+        <main className="main">{children}</main>
+        <Footer />
+      </ThemeProvider>
     </>
   );
 };
